64f97cf3d1cbf4d063100b2a5b99a6d70b64b7ce
eric <eric.thomas@formation-industries-fc.fr>
2021-11-16 14:41:55 +0100

    version 2 avec non finalisée

--- main.c
+++ main.c
@@ -1,35 +1,55 @@
 #include <stdio.h>
 #include <stdbool.h>
+/**************prototypes fonctions***********************/
+void saisieNote();
+void testGrade();
+float calculMoyenne();
+void gestionErreur();
+/*************variables globales*******************************/
+float note=0,somme,nbnotes=0;
+enum{TRES_INSUFFISANT='F',INSUFFISANT='D',PASSABLE='C',BON='B',TRES_BON='A'};
 
 int main() {
-    float note=0,somme=0,nbnotes=0;
-    enum{TRES_INSUFISSANT='F',INSUFFISANT='D',PASSABLE='C',BON='B',TRES_BON='A'};
-    while(true) {
-        note=-1; //remet à -1 pour la gestion des erreurs
-        printf("Veuillez saisir la note\n");
-        scanf("%f", &note);
-        if (note >= 14 && note <= 20) {
-            printf("La note est de grade %c\n", TRES_BON);
-        } else if (note >= 10.5 && note <= 13.9) {
-            printf("La note est de grade %c\n", BON);
-        } else if (note >= 9 && note <= 10.4) {
-            printf("La note est de grade %c\n", PASSABLE);
-        } else if (note >= 8 && note <= 8.9) {
-            printf("La note est de grade %c\n", INSUFFISANT);
-        } else if (note>=0&&note <= 7.9) {
-            printf("La note est de grade %c\n", TRES_INSUFISSANT);
-        }
-        else{ //cas où il y a une erreur
-            scanf("%*[^\n]"); //vide le scanf
-            printf("Note invalide\n");
-            continue;
-        }
-        /*****************************calcul de la moyenne****************************/
-        nbnotes++; // incrémente le nombre de notes
-        somme+=note; //calcul de la somme
-        printf("La moyenne des notes est de %0.1f\n",somme/nbnotes);
+
+    while(true)
+    {
+        saisieNote();
+        testGrade();
+        printf("La moyenne des notes est de %0.1f\n",calculMoyenne());
     }
 
 
+
+
+
     return 0;
+}
+
+void saisieNote() {
+    printf("Veuillez saisir la note\n");
+    scanf("%f",&note);
+    nbnotes++;
+    somme+=note;
+}
+
+void testGrade() {
+    if (note >= 14 && note <= 20) {
+        printf("La note est de grade %c\n", TRES_BON);
+    } else if (note >= 10.5 && note <= 13.9) {
+        printf("La note est de grade %c\n", BON);
+    } else if (note >= 9 && note <= 10.4) {
+        printf("La note est de grade %c\n", PASSABLE);
+    } else if (note >= 8 && note <= 8.9) {
+        printf("La note est de grade %c\n", INSUFFISANT);
+    } else if (note>=0&&note <= 7.9) {
+        printf("La note est de grade %c\n", TRES_INSUFFISANT);
+    }
+}
+
+float calculMoyenne() {
+    return somme/nbnotes;
+}
+
+void gestionErreur() {
+
 }
